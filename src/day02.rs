4690fe2fd07c8ceb8d6e49cc6721ab38498223f2
Annie Cherkaev <annie.cherk@gmail.com>
2020-05-03 13:29:00 -0700

    minor cleanup /
    Co-authored-by Laura Lindzey <lindzey@gmail.com>

--- src/day02.rs
+++ src/day02.rs
@@ -3,70 +3,74 @@
 #![allow(clippy::panic)]
 #![allow(clippy::indexing_slicing)]
 #![allow(clippy::integer_arithmetic)]
-#![allow(clippy::ptr_arg)]
 
 #[cfg(test)]
 use pretty_assertions::assert_eq;
 
-fn add(idx: usize, data: &mut Vec<usize>) -> usize {
+/// Writes data[pc+1] + data[pc+2] to data[pc+3], returns new program counter
+/// pc is the address of the add opcode being executed
+fn add(pc: usize, memory: &mut Vec<usize>) -> usize {
     // Opcode 1 adds together numbers read from two positions and stores the
     // result in a third position. The three integers immediately after the
     // opcode tell you these three positions - the first two indicate the
     // positions from which you should read the input values, and the third
     // indicates the position at which the output should be stored.
-    let idx1 = data[idx + 1];
-    let idx2 = data[idx + 2];
-    let idx3 = data[idx + 3];
-    data[idx3] = data[idx1] + data[idx2];
-    idx + 4
+    let arg1_addr = memory[pc + 1];
+    let arg2_addr = memory[pc + 2];
+    let result_addr = memory[pc + 3];
+    memory[result_addr] = memory[arg1_addr] + memory[arg2_addr];
+    pc + 4
 }
 
-/// Input `idx` is the current index of the opcode
-fn mult(idx: usize, data: &mut Vec<usize>) -> usize {
+/// Writes data[pc+1] * data[pc+2] to data[pc+3], returns new program counter
+/// pc is the address of the mult opcode being executed
+fn mult(pc: usize, memory: &mut Vec<usize>) -> usize {
     // Opcode 2 works exactly like opcode 1, except it multiplies the two
     // inputs instead of adding them. Again, the three integers after the
     // opcode indicate where the inputs and outputs are, not their values.
-    let idx1 = data[idx + 1];
-    let idx2 = data[idx + 2];
-    let idx3 = data[idx + 3];
-    data[idx3] = data[idx2] * data[idx1];
-    idx + 4
+    let arg1_addr = memory[pc + 1];
+    let arg2_addr = memory[pc + 2];
+    let result_addr = memory[pc + 3];
+    memory[result_addr] = memory[arg1_addr] * memory[arg2_addr];
+    pc + 4
 }
 
 /// Public facing entry point that is called in main.rs to get the answers.
 pub fn answers() -> (usize, usize) {
-    let filestr = include_str!("../inputs/day02.txt");
-    let mut input = parse(filestr);
-    input[1] = 12;
-    input[2] = 2;
-    let answer1 = run_computer(&input);
-    let answer2 = part2(&input);
+    let day_02_contents = include_str!("../inputs/day02.txt");
+    let mut memory = parse(day_02_contents);
+    memory[1] = 12;
+    memory[2] = 2;
+    let answer1 = run_computer(&memory);
+    let answer2 = part2(&memory);
     (answer1[0], answer2)
 }
 
-fn run_computer(input: &Vec<usize>) -> Vec<usize> {
-    let mut idx = 0;
-    let mut program = input.clone();
+/// Intcode interpretter
+/// Copies the contents of `input_memory` and then evaluates the program
+/// starting the program counter at 0.
+fn run_computer(input_memory: &[usize]) -> Vec<usize> {
+    let mut pc = 0;
+    let mut memory = input_memory.to_owned();
     loop {
-        match program[idx] {
-            1 => idx = add(idx, &mut program),
-            2 => idx = mult(idx, &mut program),
+        match memory[pc] {
+            1 => pc = add(pc, &mut memory),
+            2 => pc = mult(pc, &mut memory),
             99 => break,
-            _ => panic!(format!("🔥🔥Unrecognized opcode {}🔥🔥", program[idx])),
+            _ => panic!(format!("🔥🔥Unrecognized opcode {}🔥🔥", memory[pc])),
         }
     }
-    program
+    memory
 }
 
-fn part2(input: &Vec<usize>) -> usize {
-    // TODO: How to do this with immutable input? Each run needs to be
-    // restarted as if it was the first one run.
-    for noun in (0..100) {
-        for verb in (0..100) {
-            let mut program = input.clone();
-            program[1] = noun;
-            program[2] = verb;
-            let result = run_computer(&program);
+/// Determines what pair of inputs produces the output 19690720
+fn part2(input_memory: &[usize]) -> usize {
+    for noun in 0..100 {
+        for verb in 0..100 {
+            let mut memory = input_memory.to_owned();
+            memory[1] = noun;
+            memory[2] = verb;
+            let result = run_computer(&memory);
             let answer = result[0];
             // This magic number is the same for all puzzles.
             if 19_690_720 == answer {
@@ -74,11 +78,11 @@ fn part2(input: &Vec<usize>) -> usize {
             }
         }
     }
-    // TODO: Return an error? We didn't find an answer...
     panic!("Did not find answer!");
 }
 
 /// Parses the day02 input
+/// Day02 input is a comma seperated list of integers.
 fn parse(data: &str) -> Vec<usize> {
     data.split(',')
         .map(|l| l.parse::<usize>().expect("Could not parse integer"))
@@ -86,7 +90,7 @@ fn parse(data: &str) -> Vec<usize> {
 }
 
 #[test]
-fn test_part_1() {
+fn test_run_computer() {
     // 1,0,0,0,99 becomes 2,0,0,0,99 (1 + 1 = 2).
     let input1 = vec![1, 0, 0, 0, 99];
     let answer1 = vec![2, 0, 0, 0, 99];
